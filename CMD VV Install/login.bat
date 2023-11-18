09c1db05c5b5ebd20dacdcc1aad19ee86196f036
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-11-18 19:00:52 +0700

    Install cmd vv

---
+++ CMD VV Install/login.bat
@@ -0,0 +1,27 @@
+echo login now
+@echo off 
+:login
+cls
+color a 
+echo  login? (Anwer in only yes/no)
+set /p input=
+if /i %input%==Yes goto ddf
+if /i %input%==No goto 89253502
+(if /i not %input%==Yes,No
+goto login)
+:ddf
+echo password (392049281/89253502)
+set /p input=
+if /i %input%==392049281 goto 392049281
+if /i %input%==89253502 goto 89253502
+(if /i not %input%==392049281,89253502
+goto login)
+pause
+
+:392049281
+color b
+cls
+git https://github.com/rraapt66/CMD-VVbysiwat.git
+cd CMD-VV-1.1.exe
+cls
+pause
