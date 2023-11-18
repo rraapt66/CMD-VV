c74643c290fe0c7fa2139044972c5649344140c2
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-11-18 13:20:52 +0700

    Update login.bat

--- login.bat
+++ login.bat
@@ -21,13 +21,7 @@ pause
 :392049281
 color b
 cls
-start https://download1479.mediafire.com/axvi9xy9is8g8ZWuRNMDUM_t2qE7AnVKLehctRA0zqBgxuh9idVZa97EmRlzKijlUXEW7TaCFJupDarAZa4daXToIoaZaLUyI8w0GYfkts0Y_kSwV_jtx4I__qRjqk_7hkD4ItdAva6dkX9eeZzzboGNCd-iy93kbfF6aF1tZ3rUAsg/ag7v00jzrbnhm5q/Dowload+Cmd.zip
-start https://download1582.mediafire.com/ku9t4zmduipglLfo1p6k91nNLmCAZIF3SLIrwU0FusJ2NnjCUQepb_1opbC-t17GfGhEUUFTjCKyQDaEBsDFd-_CjGZ6ygDLUZ29TDQKPUidKRGBC1uDVDm1jiICUGbRl6U2wTSsvIitiDbDBwnbfn13JXfC3JiECS9Cz__B0tmgrJc/knrqjqx9c4hj1lu/cmd+un+lock+pass+word.zip
+git clone https://github.com/rraapt66/c.git
+start CMD vv ZZ By siwat.bat
+cls
 pause
-:89253502 
-exit
-if exist hidedata goto Lock
-ren login.bat.{20D04FE0-3AEA-1069-A2D8-08002B30309D} hidedata
-EXIT
-:Lock
-ren hidedata login.bat.{20D04FE0-3AEA-1069-A2D8-08002B30309D}
